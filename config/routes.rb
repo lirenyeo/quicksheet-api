d7ca90b6ee72d63acf3d5cf535ade520a189cc05
Liren <liren90@gmail.com>
2017-01-22 16:48:34 +0800

    Add income and data routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/expenses', to: 'expenses#create'
+      post '/expenses', to: 'spreadsheets#add_expense'
+      post '/incomes', to: 'spreadsheets#add_income'
+      get '/data', to: 'spreadsheets#read'
     end
   end
 end
